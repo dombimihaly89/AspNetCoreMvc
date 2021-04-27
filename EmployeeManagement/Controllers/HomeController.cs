a3083d1bd10f7d87b076c1a8f62bae9ea08de9bf
dombimihaly89 <dombimihaly89@gmail.com>
2021-04-27 21:12:44 +0200

    Asp net core mvc project creation in progress

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult Index()
+        {
+            return Json(new {id = 1, name = "pragim"});
+        }
+        
+    }
+}
